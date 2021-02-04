afecac0bbb0e345098b9e36ae39d5ce8445eb7b6
user <user@JasonMac.local>
2021-02-04 19:01:34 +1030

    update

--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@ import requests
 import re
 from bs4 import BeautifulSoup
 from datetime import datetime
-
+import pandas as pd
 
 def iterate_re(tr, endtype, starttype):
 	result = ""
@@ -94,12 +94,65 @@ def scrape_xlotto_3():
 			file_object.write(iterate_re(tr, '\t', 'Wednesday'))
 	file_object.close()
 
+def scrape_flash_select_powerball():
+	f = open("flash-select-powerball.html", 'r')
+	df = pd.DataFrame(columns=['num1', 'num2', 'num3', 'num4', 'num5', 'num6', 'num7', 'powerball'])
+
+	html = f.read()
+	names = re.finditer(r"<div class=\"numbers\">.+?PB.+?</div>", html, flags = re.MULTILINE)
+	index = 0
+	for tr in names:
+		entry = tr.group()
+		rows = entry.split('\n')
+		row = rows[0]
+
+		numbers = re.finditer(r"<span(.|\n)*?au-target-id.*?[0-9]+\">[0-9]{1,2}</span>", row, flags = re.MULTILINE)
+		result = []
+		for num in numbers:
+			n = num.group()
+			things = re.compile("<span(.|\n)*?au-target-id.+?>").split(n)
+			target = things[2].replace("</span>", "")
+			result.append(target)
+		
+		nums = result[0:7]
+		powerball = result[7]
+		nums = [int(x) for x in nums]
+		nums.sort()
+		nums.append(powerball)
+		df.loc[index] = nums
+		index += 1
+	print(df)
+	df.to_csv("powerball-flash-result.csv", index=False, mode='a', header=False)
+	f.close()
+
 # def combine_filter_lotto(arr1, arr2, arr3, arr4, arr5, arr6):
 
+# def summation(a, b):
+# 	result = a + b
+# 	# print(result)
+# 	if result == 11:
+# 		return result
+# 		print("123")
+# 	else:
+# 		print("321")
+# 		return 4
+# 	print("345")
 
+
+def strip_comma(text):
+	print(text.replace(",", ""))
 
 def main():
-    scrape_powerball()
+    # scrape_flash_select_powerball()
+    # n3 = n2 + n1
+    d = {}
+
+    d["a"] = {"b": [ {1:"n"} , {2:["c", "b"]} ] }
+    d["jose"] = ["amazon", "taobao", "alibaba"]
+
+    print(d["a"]["b"][1][2][1])
+
+
 
 if __name__ == "__main__":
     main()
